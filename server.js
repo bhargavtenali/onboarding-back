b301fba97f2ee36f22609d5abfcfa48e61ccf9b8
Bhargav Tenali <bhargavtenali@gmail.com>
2024-10-04 19:09:29 +0530

    fixed minor issues

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
 const authRoutes = require("./src/routes/authRoutes");
 const employeeRoutes = require("./src/routes/employeeRoutes");
 const taskRoutes = require("./src/routes/taskRoutes");
 const errorHandler = require("./src/middleware/errorHandler");
+const connectDB = require("./src/config/database");
+
+dotenv.config();
 
 const app = express();
+
+connectDB();
 
 app.use(cors());
 app.use(express.json());
@@ -15,5 +21,12 @@ app.use("/api/employees", employeeRoutes);
 app.use("/api/tasks", taskRoutes);
 
 app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 module.exports = app;
